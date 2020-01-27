811cf31cc88a2df7536a775659bbce210396af85
demerll <58672347+demerll@users.noreply.github.com>
2020-01-26 18:10:57 -0600

    Completed Assignment

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--Index.php
+Assignment 6
+INFO 2340 -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Assignment 6</title>
+</head>
+<body>
+<h1> Hello, world</h1>
+<div>Allow me to inroduce myself! My name is <?php echo "Donjeta Emerllahu";?> </div>
+<div>I am <?php echo "25";?> years old and I am a student at Metropolitain Community College.</div>
+<div>I am currently studying <?php echo "Web Development";?> and I hope to graduate in <?php echo "2020";?>.</div>
+    
+</body>
+</html>
